22967409061f1e444b7b15091c088fe44007a9bd
Marsikus93 <marusia234@gmail.com>
2022-11-25 18:20:02 +0200

    new4

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-type PropsType={
-  body:string
-  userId:number
-  id: number
-  title:string
+
+type PropsType = {
+    body: string
+    userId: number
+    id: number
+    title: string
 }
 
 function App() {
-  const[state,setState]=useState<PropsType[]>([])
-  console.log(state)
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(json => setState(json))
-  },[])
-const deleteHandler=()=>{
-      setState([])
-}
-
-
-
-
-    const postsHandler=()=>{
+    const [state, setState] = useState<PropsType[]>([])
+    console.log(state)
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setState(json))
+    }, [])
+    const deleteHandler = () => {
+        setState([])
+    }
+    const postsHandler = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(json => setState(json))
     }
-  return (
-    <div className="App">
-      <button onClick={deleteHandler}>Delete</button>
-      <button onClick={postsHandler}>ShowPosts</button>
-      <ul>
-      {state.map((el)=>{
-      return(
+    return (
+        <div className="App">
+            <button onClick={deleteHandler}>Delete</button>
+            <button onClick={postsHandler}>ShowPosts</button>
+            <ul>
+                {state.map((el) => {
+                    return (
 
-            <li key={el.id}>
-              <span>{el.id}</span>
-              <span>{el.title}</span>
-              <span>{el.userId}</span>
+                        <li key={el.id}>
+                            <span>{el.id}</span>
+                            <span>{el.title}</span>
+                            <span>{el.userId}</span>
 
-            </li>
-      )
-      })}
-      </ul>
-    </div>
-  );
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
