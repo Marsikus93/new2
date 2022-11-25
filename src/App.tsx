e5c1c11e12e577cebf5e9c7e9a366f2cc0a2275b
Marsikus93 <marusia234@gmail.com>
2022-11-25 18:16:21 +0200

    new3

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,9 @@ const deleteHandler=()=>{
       setState([])
 }
 
+
+
+
     const postsHandler=()=>{
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
